af221fd1711a7cb3eaa11920828d2706021a78f8
holiv <mrheltonso@gmail.com>
2023-05-06 17:07:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Minimum Value Challenge
+### Program that finds the minimum value in an array of ten elements from the user input.
+
+1. The user must input a list of 10 numeric elements separated by comma.
+2. If the list of elements is less or greater than 10 the user will be notified to input a list of current length.
+3. If at least one of the elements is a non numeric element, the user will be notified about the invalid character and will be asked to input a correct list.
+4. If everything's perfect the minimum value will be printed in the console.
---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        readIntegers();
+    }
+
+    public static int[] readIntegers() {
+        Scanner scanner = new Scanner(System.in);
+
+        String numList;
+        int[] numArr = new int[10];
+        do {
+            System.out.println("Enter a list of 10 numbers separated by comma");
+            numList = scanner.nextLine();
+        } while (validation(numList));
+
+        String[] splitList = numList.split(",");
+        for (int i = 0; i < splitList.length; i++) {
+            numArr[i] = Integer.parseInt(splitList[i]);
+        }
+        System.out.println("The min value on the current array is " + findMin(numArr));
+        return numArr;
+    }
+
+    public static boolean validation(String numList){
+        if (numList.split(",").length == 10) {
+            String[] numArr = numList.split(",");
+            for (String element : numArr) {
+                if (!isNumeric(element)) {
+                    System.out.println("Invalid character in the list of numbers");
+                    return true;
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isNumeric (String character) {
+        try {
+            Integer.parseInt(character);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+
+    public static int findMin(int[] arr) {
+        Arrays.sort(arr);
+        return arr[0];
+    }
+}
